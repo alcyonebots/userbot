84964e05366cda166e21885834429e161792bdad
alcyonebots <evedhatachi@gmail.com>
2025-01-20 17:19:52 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,16 @@
+import pymongo
+
+# MongoDB setup
+client = pymongo.MongoClient("mongodb+srv://Cenzo:Cenzo123@cenzo.azbk1.mongodb.net/")
+db = client["reaper"]
+sessions_collection = db["sessions"]
+
+# Load quotes from a file
+def load_quotes():
+    quotes_file = 'chudai.txt'
+    if os.path.exists(quotes_file):
+        with open(quotes_file, 'r') as file:
+            return [line.strip() for line in file.readlines()]
+    return ["No quotes found."]
+
+quotes = load_quotes()
